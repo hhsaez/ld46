956d61395d78ab0a0d511fbfa4b8e763df3bf3af
Hernan Saez <hhsaez@gmail.com>
2020-04-19 01:13:26 -0300

    Enemy death

--- Assets/Scripts/Components/Actor.cs
+++ Assets/Scripts/Components/Actor.cs
@@ -15,15 +15,37 @@ namespace ld46.Components {
         [ SerializeField ] protected FloatValue m_oponentHealth;
 
         private Animator m_animator;
-        
+
         void Start()
         {
             m_animator = GetComponent< Animator >();
             m_health.Value = m_maxHealth.Value;
         }
 
+        private void OnEnable() 
+        {
+            if ( m_health != null ) {
+                m_health.OnValueChanged += this.OnHealthChanged;
+            }
+        }
+
+        private void OnDisable() 
+        {
+            if ( m_health != null ) {
+                m_health.OnValueChanged -= this.OnHealthChanged;
+            }
+        }
+
+        private void OnHealthChanged( FloatValue value )
+        {
+            if ( m_health.Value <= 0 ) {
+                m_animator.SetTrigger( "isDead" );     
+                this.enabled = false;
+            }
+        }
+
         public void Attack( float damage )
-        {            
+        {
             m_oponentHealth.Value -= damage;
         }
 
